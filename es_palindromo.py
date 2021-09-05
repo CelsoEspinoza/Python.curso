19c1765931344b0abff11cb01231d8389898442c
CelsoEspinoza <c.espinoza.arce@outlook.com>
2021-09-05 18:39:38 -0500

    Mejorando  función palíndromo

--- es_palindromo.py
+++ es_palindromo.py
@@ -1,12 +1,16 @@
+def is_palindrome(text):
+    text = text.lower().replace(" ", "")
+    inverted_text = text[::-1]
+    return inverted_text == text
 
 
 def main():
-    palabra = input("Escribe la palabra a evaluar: ")
-    reversa = palabra[::-1]
-    if palabra == reversa:
-        print("La palabra {} es palíndroma".format(palabra))
+    palabra = input("Escribe el texto a evaluar: ")
+    es_palindromo = is_palindrome(palabra)
+    if es_palindromo:
+        print("El texto \"{}\" es palíndromo".format(palabra))
     else:
-        print("La palabra no es palíndroma")
+        print("El texto \"{}\" NO es palíndromo".format(palabra))
 
     input("")
 
